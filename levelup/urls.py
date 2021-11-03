2c29654c7eded1e17a35506a6e4611799e247892
Matthew Singler <matthew.singler@gmail.com>
2021-11-03 16:04:34 -0500

    serializer and View

--- levelup/urls.py
+++ levelup/urls.py
@@ -4,22 +4,18 @@ from django.contrib import admin
 from django.conf.urls import include
 from django.urls import path
 from levelupapi.views import register_user, login_user
+from levelupapi.views import GameTypeView, GameView, EventView
+
 
 router = routers.DefaultRouter(trailing_slash=False)
 router.register(r'gametypes', GameTypeView, 'gametype')
+router.register(r'games', GameView, 'game')
+router.register(r'events', EventView, 'event')
 
 urlpatterns = [
     path('', include(router.urls)),
     path('register', register_user),
     path('login', login_user),
     path('api-auth', include('rest_framework.urls', namespace='rest_framework')),
-]
-
-urlpatterns = [
-    # Requests to http://localhost:8000/register will be routed to the register_user function
-    path('register', register_user),
-    # Requests to http://localhost:8000/login will be routed to the login_user function
-    path('login', login_user),
-    path('api-auth', include('rest_framework.urls', namespace='rest_framework')),
-    path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls)
 ]
---
+++ levelupapi/views/event.py
@@ -0,0 +1,57 @@
+from django.http import HttpResponseServerError
+from rest_framework.viewsets import ViewSet
+from rest_framework.response import Response
+from rest_framework import serializers
+from levelupapi.models import Event, EventGamer, event_gamer, Game, Gamer, gamer
+from levelupapi.views.game import GameSerializer
+from rest_framework import status
+from django.core.exceptions import ValidationError
+
+
+class EventView(ViewSet):
+    """Level up events"""
+
+    def list(self, request):
+        events = Event.objects.all()
+        game = self.request.query_params.get("game")
+        gamer = Gamer.objects.get(user=request.auth.user)
+        if game is not None:
+            events = events.filter(game__id=game)
+
+        serializer = EventSerializer(
+            events, many=True, context={'request': request})
+        return Response(serializer.data)
+    
+    def create(self, request):
+        event = Event.objects.get(user=request.auth.user)
+        game = Game.objects.get(pk=request.data["game"])
+        event = Event.objects.create(
+            description=request.data["description"],
+            date=request.data["date"],
+            time=request.data["time"],
+            organizer=gamer,
+            game=game
+            )
+        try:
+            event.save()
+            serializer = EventSerializer(event, context={'request': request})
+            return Response(serializer.data)
+
+        except ValidationError as ex:
+            return Response({"reason": ex.message}, status=status.HTTP_400_BAD_REQUEST)
+
+
+class EventGamerSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Gamer
+        fields = ("user", "bio")
+
+class EventSerializer(serializers.ModelSerializer):
+    organizer = EventGamerSerializer()
+    game = GameSerializer()
+
+
+    class Meta:
+        model = Event
+        fields = ("id", "game", "organizer", "description", "date", "time")
