889dc0643cd1a1206f6fe1afc290b5f25ff57616
Matthew Singler <matthew.singler@gmail.com>
2021-11-03 10:37:28 -0500

    viewset and serializer

--- levelup/urls.py
+++ levelup/urls.py
@@ -1,7 +1,19 @@
+from rest_framework import routers
+from levelupapi.views import GameTypeView
 from django.contrib import admin
 from django.conf.urls import include
 from django.urls import path
 from levelupapi.views import register_user, login_user
+
+router = routers.DefaultRouter(trailing_slash=False)
+router.register(r'gametypes', GameTypeView, 'gametype')
+
+urlpatterns = [
+    path('', include(router.urls)),
+    path('register', register_user),
+    path('login', login_user),
+    path('api-auth', include('rest_framework.urls', namespace='rest_framework')),
+]
 
 urlpatterns = [
     # Requests to http://localhost:8000/register will be routed to the register_user function
